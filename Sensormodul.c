cc661d23d0d3491d1f998425ca6d9ece2362402d
PatrikSundin <patsu326@student.liu.se>
2014-04-16 15:23:44 +0200

    Sensor: volatile!

--- Sensormodul.c
+++ Sensormodul.c
@@ -2,6 +2,20 @@
 #include <avr/interrupt.h>
 
 
+// Namn istället för massa jobbiga siffror /Robert
+char front = 0b00000001;
+char rightfront = 0b00000010;
+char rightback = 0b00000011;
+char leftfront = 0b00000100;
+char leftback = 0b00000101;
+char traveldist = 0b00000110;
+char gyro = 0b00000111;
+char RFID = 0b00001000;
+char stop = 0x00; //Stopbyte
+
+char selection; // Används i skicka avbrottet
+
+
 char i = 0; //Vilken sensor jag använder
 char m = 0; //Hur många gånger jag har gått igenom sensorn.
 char calibrated = 0;
@@ -26,6 +40,8 @@ float dummy;
 long n = 0;
 long timer = 0;
 long j = 0;
+char start_sample = 0;
+volatile char ad_complete = 0;
 
 void bubble_sort(unsigned char a[], int size)
 {
@@ -43,6 +59,30 @@ void bubble_sort(unsigned char a[], int size)
 		}
 	}
 }
+
+
+void SlaveInit(void)
+{
+	/* Set MISO output, all others input */
+	DDRB = (1<<DDB6);
+
+	/* Enable SPI */
+	SPCR = (1<<SPE)|(1<<SPIE);
+
+	/* Enable External Interrupt */
+//	sei();
+}
+
+char SlaveRecieve(void) // Används inte just nu men....
+{
+	/*Wait for reception complete */
+	while(!(SPSR & (1<<SPIF)))
+	;
+
+	/* Return Data Register */
+	return SPDR;
+}
+
 
 void USART_Init( unsigned int baud )
 {
@@ -78,8 +118,14 @@ void initiate_sensormodul(void)
 	sei();
 	USART_Init(25);
 	UCSR0B = (0<<RXEN0)|(0<<TXEN0); //Stäng av USART
-	
+
 	ADCSRA = 0b11001011;
+	ADMUX = 0;
+
+	TCCR0B = 0x00; //stop
+	TCNT0 = 0x00; //set count
+	OCR0B  = 0x04;  //set compare
+	TCCR0B = 0x03; //start timer
 }
 
 void find_RFID(void) //Vet inte riktigt hur vi ska leta RFID, men det är ett mycket senare problem.
@@ -95,27 +141,175 @@ void find_RFID(void) //Vet inte riktigt hur vi ska leta RFID, men det är ett my
 	}
 }
 
+void initiate_sample_timer()
+{
+	TIMSK1 = 0b00000100; //Enable interupt vid matchning med OCR1B		TCCR1B =0x00;
+	TCNT1 = 0x00;
+	//TCCR1B = 0x03; //Starta samplingsräknare, presscale 64.
+	OCR1BH = 0x00;
+	OCR1BL = 0x60; //RANDOM! När ska comparen triggas? SAMPLING
+}
+
 int main(void)
 {
-
+	SlaveInit();
 	initiate_sensormodul();
+	initiate_sample_timer();
 	while(1)
 	{
-		/*ADCSRA = 0b11001011;
-		find_RFID();*/
+		if(start_sample == 1)
+		{
+			start_sample = 0;
+			//find_RFID();
+		}
+		else
+		{
+			dummy = 1;
+		}
 		
+		asm("");
+		
+		if(ad_complete == 1)
+		{	
+			ad_complete = 0;
+
+			if(calibrated == 0)
+			{
+				gyroref = ADC >> 2;
+				ADMUX = 0;
+				calibrated = 1;
+				TCCR1B = 0x03;
+			}
+			else
+			{
+				if(i == 0)
+				{
+					dFront[m] = ADC >> 2;
+					if (m == 9)
+					{
+						bubble_sort(dFront, 10);
+						sortedValues[0] = dFront[2];
+						i = i + 1;
+						m = 0;
+						ADMUX = i;
+					}
+					else
+					{
+						m = m + 1;
+					}
+				}
+				if(i == 1)
+				{
+					dRight_Front[m] = ADC >> 2;
+					if (m == 9)
+					{
+						bubble_sort(dRight_Front, 10);
+						sortedValues[1] = dRight_Front[2];
+						i = i + 1;
+						m = 0;
+						ADMUX = i;
+					}
+					else
+					{
+						m = m + 1;
+					}
+				}
+				if(i == 2)
+				{
+					dRight_Back[m] = ADC >> 2;
+					if (m == 9)
+					{
+						bubble_sort(dRight_Back, 10);
+						sortedValues[2] = dRight_Back[2];
+						i = i + 1;
+						m = 0;
+						ADMUX = i;
+					}
+					else
+					{
+						m = m + 1;
+					}
+				}
+				if(i == 3)
+				{
+					dLeft_Front[m] = ADC >> 2;
+					if (m == 9)
+					{
+						bubble_sort(dLeft_Front, 10);
+						sortedValues[3] = dLeft_Front[2];
+						i = i + 1;
+						m = 0;
+						ADMUX = i;
+					}
+					else
+					{
+						m = m + 1;
+					}
+				}
+				if(i ==	4)
+				{
+					dLeft_Back[m] = ADC >> 2;
+					if (m == 9)
+					{
+						bubble_sort(dLeft_Back, 10);
+						sortedValues[4] = dLeft_Back[2];
+						i = i + 1;
+						m = 0;
+						ADMUX = i;
+					}
+					else
+					{
+						m = m + 1;
+					}
+				}
+				if(i == 5)
+				{
+					tempDistance = dDist;
+					dDist = ADC >> 2;
+					if (((tempDistance <= 150) && (dDist > 150)) | ((tempDistance >= 150) && (dDist < 150)))
+					{
+						Distance = Distance + 1;
+					}
+					i = i + 1;
+					ADMUX = i;
+				}
+				if(i == 6)
+				{
+					dGyro = ADC >> 2;
+					if ((dGyro < gyroref + 2) && (dGyro > gyroref - 2))
+					{
+						sendGyro = sendGyro;
+					}
+					else
+					{
+						dGyro= dGyro - gyroref;
+						timer = TCNT0;
+						sendGyro = sendGyro + dGyro * timer; //64 är prescalen på timern
+						TCNT0 = 0x00; //set count
+					}
+					i = 0;
+					ADMUX = i;
+				}
+			}
+			TCCR1B = 0x03;
+		}
 	}
+}
+
+ISR(TIMER1_COMPB_vect)
+{
+	
+	TCCR1B = 0x00;
+	TCNT1 = 0x00;
+	start_sample = 1;
+	ADCSRA = 0b11001011;	
 }
 
 
 ISR(INT0_vect) //knapp ska vi inte ha irl, men ja.
 {
-	ADMUX = 0;
 	ADCSRA = 0b11001011;
-	TCCR0B = 0x00; //stop
-	TCNT0 = 0x00; //set count
-	OCR0B  = 0x04;  //set compare
-	TCCR0B = 0x03; //start timer
+	//:)
 }
 
 
@@ -123,180 +317,63 @@ ISR(INT0_vect) //knapp ska vi inte ha irl, men ja.
 
 ISR(ADC_vect)
 {
-	if(calibrated == 0)
+	ADCSRA = 0b10001011;
+	ad_complete = 1;
+	
+}
+
+
+
+ISR(SPI_STC_vect) // Skicka på buss!! // Robert
+{
+	char selection = SPDR;
+
+	if(selection == front)
 	{
-		gyroref = ADC >> 2;
-		ADMUX = 0;
-		ADCSRA = 0b10001011;
-		calibrated = 1;
+		SPDR = sortedValues[0];
 	}
-	else
+	else if (selection == rightfront)
 	{
-		if(i == 0)
-		{
-			dFront[m] = ADC >> 2;
-		}
-		if(i == 1)
-		{
-			dRight_Front[m] = ADC >> 2;
-		}
-		if(i == 2)
-		{
-			dRight_Back[m] = ADC >> 2;
-		}
-		if(i == 3)
-		{
-			dLeft_Front[m] = ADC >> 2;
-		}
-		if(i == 4)
-		{
-			dLeft_Back[m] = ADC >> 2;
-		}
-		if(i == 5)
-		{
-			tempDistance = dDist;
-			dDist = ADC >> 2;
-			if (((tempDistance <= 150) && (dDist > 150)) | ((tempDistance >= 150) && (dDist < 150)))
-			{
-				Distance = Distance + 1;
-			}
-		}
-		if(i == 6)
-		{
-			dGyro = ADC >> 2;
-			if ((dGyro < gyroref + 2) && (dGyro > gyroref - 2))
-			{
-				sendGyro = sendGyro;
-			}
-			else
-			{
-				dGyro= dGyro - gyroref;
-				timer = TCNT0;
-				sendGyro = sendGyro + dGyro * timer; //64 är prescalen på timern
-				TCNT0 = 0x00; //set count
-			}
-		}
-		if(i == 6)
-		{
-			i = 0;
-			m = 0;
-			bubble_sort(dFront, 10);
-			bubble_sort(dRight_Front, 10);
-			bubble_sort(dRight_Back, 10);
-			bubble_sort(dLeft_Front, 10);
-			bubble_sort(dLeft_Back, 10);
-			sortedValues[0] = dFront[2];
-			sortedValues[1] = dRight_Front[2];
-			sortedValues[2] = dRight_Back[2];
-			sortedValues[3] = dLeft_Front[2];
-			sortedValues[4] = dLeft_Back[2];
-			ADCSRA = 0b10001011;
-		}
-		else
-		{
-			if (m == 9)
-			{
-				i = i + 1;
-				ADMUX = i;
-				m = 0;
-			}
-			else
-			{
-				if(i < 5)
-				{
-					m = m + 1;
-				}
-				else
-				{
-					i = i + 1;
-					ADMUX = i;
-				}
-			}
-			ADCSRA = 0b11001011;
-			ADMUX = 0;
-		}
+		SPDR = sortedValues[1];
+	}
+	else if (selection == rightback)
+	{
+		SPDR = sortedValues[2];
+	}
+	else if (selection == leftfront)
+	{
+		SPDR = sortedValues[3];
+	}
+	else if (selection == leftback)
+	{
+		SPDR = sortedValues[4];
+	}
+	else if (selection == traveldist)
+	{
+		SPDR = Distance;
+		asm("");
+		Distance = 0;
+	}
+	else if (selection == gyro)
+	{
+		SPDR = sendGyro;
+		asm("");
+		sendGyro = 0;
+	}
+	else if (selection == RFID)
+	{
+		SPDR = isRFID;
+	}
+	else if (selection == stop)
+	{
+		// behöver förmodligen inte göra något här
 	}
 }
 
 
-void send_front()
-{
-	//skicka f?rst 1;
-	//sortedValues[0];
-}
 
-void send_right_front()
-{
-	//skicka f?rst 2;
-	//sortedValues[1];
-}
 
-void send_right_back()
-{
-	//skicka f?rst 3;
-	//sortedValues[2];
-}
 
-void send_left_front()
-{
-	//skicka f?rst 4;
-	//sortedValues[3];
-}
-
-void send_left_back()
-{
-	//skicka f?rst 5;
-	//sortedValues[4];
-}
-
-void send_dist()
-{
-	//skicka f?rst 6;
-	//Distance;
-	Distance = 0;
-}
-
-void send_gyro()
-{
-	//skicka f?rst 7;
-	//sendGyro;
-	sendGyro = 0;
-}
-
-void send_RFID()
-{
-	//skicka f?rst 8;
-	//isRFID;
-	isRFID = 0;
-}
-
-void convert_front()
-{
-}
-
-void convert_fight_front()
-{
-}
-
-void convert_right_back()
-{
-}
-
-void convert_left_front()
-{
-}
-
-void convert_left_back()
-{
-}
-
-void convert_dist()
-{
-}
-
-void convert_gyro()
-{
-}
 
 /* GAMMALT SOM HAR ANVÄNTS I TESTSYFTE */
 
